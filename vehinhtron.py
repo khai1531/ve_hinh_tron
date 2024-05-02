a405f64b650f8f08a992dc1064b5abf053b93e5b
khai1531 <165395791+khai1531@users.noreply.github.com>
2024-05-02 15:05:53 +0700

    Add files via upload

---
+++ vehinhtron.py
@@ -0,0 +1,13 @@
+import turtle
+
+turtle.pensize (5)
+
+turtle.pencolor ("violet")
+
+turtle.Turtle().goto(-40, 120)
+
+turtle.fillcolor ("violet")
+turtle.begin_fill()
+
+turtle.circle (150)
+turtle.end_fill()
